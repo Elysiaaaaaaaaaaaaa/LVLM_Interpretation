2af5762b58862794c6883583bd3f4c590f7b904b
czx_0000 <czx_0000@qq.com>
2026-04-01 17:01:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-sudo pip3 uninstall flash-attn timm
+sudo pip3 uninstall -y flash-attn timm
 
-pip3 install -e ".[train]"
-# pip3 install flash-attn --no-build-isolation
-pip3 install ipdb tensorboard openai openpyxl datasets pytesseract decord mamba-ssm causal-conv1d
-pip3 install -U pillow
+pip3 install -y -e ".[train]"
+# pip3 install -y flash-attn --no-build-isolation
+pip3 install -y ipdb tensorboard openai openpyxl datasets pytesseract decord mamba-ssm causal-conv1d
+pip3 install -y -U pillow
 
-pip3 install matplotlib yake
-pip3 install open_clip_torch diffusers[torch] ezcolorlog
-pip3 install timm==0.9.16
-pip3 install opencv-python
-pip3 install datasets pandas
+pip3 install -y matplotlib yake
+pip3 install -y open_clip_torch diffusers[torch] ezcolorlog
+pip3 install -y timm==0.9.16
+pip3 install -y opencv-python
+pip3 install -y datasets pandas
