ca6ebb1f658664e5165a331370e19060001e2b9f
czx_0000 <czx_0000@qq.com>
2026-04-03 21:41:47 +0800

    test more exprement parameters

--- Advanced_IGOS_PP/IGOS_pp.py
+++ Advanced_IGOS_PP/IGOS_pp.py
@@ -150,8 +150,8 @@ def gen_explanations_qwenvl(model, processor, image, text_prompt, tokenizer, pos
     L3 = 10.0
     momentum = 5
     ig_iter = 10
-    iterations=5
-    lr=10
+    iterations=10
+    lr=1
     
     method = iGOS_pp
     
