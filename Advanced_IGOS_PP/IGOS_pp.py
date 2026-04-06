33e7fcda1b5ab2090dbe1269e35a24aeb1c45c96
czx_0000 <czx_0000@qq.com>
2026-04-06 13:46:12 +0800

    Update IGOS_pp.py

--- Advanced_IGOS_PP/IGOS_pp.py
+++ Advanced_IGOS_PP/IGOS_pp.py
@@ -140,18 +140,18 @@ def gen_explanations_qwenvl(model, processor, image, text_prompt, tokenizer, pos
     # iterations=10
     # lr=1.0  # 降低学习率以防止梯度爆炸
 
-    size=16
+    size=32
     opt = 'NAG'
     diverse_k = 1
     init_posi = 0
-    init_val = 0.4
-    L1 = 2.0
+    init_val = 0.3
+    L1 = 1.5
     L2 = 8.0
     gamma = 1.0
     L3 = 18.0
     momentum = 0.8
     ig_iter = 10
-    iterations=25
+    iterations=20
     lr=0.02
     
     method = iGOS_pp
