2aba203b9312d658481792da74b4afc806443438
czx_0000 <czx_0000@qq.com>
2026-04-22 16:38:26 +0800

    Update IGOS_pp.py

--- Advanced_IGOS_PP/IGOS_pp.py
+++ Advanced_IGOS_PP/IGOS_pp.py
@@ -151,8 +151,8 @@ def gen_explanations_qwenvl(model, processor, image, text_prompt, tokenizer, pos
     L3 = 30
     momentum = 0.8
     ig_iter = 10
-    iterations=30
-    lr=0.01
+    iterations=40
+    lr=0.005
     
     method = iGOS_pp
     
